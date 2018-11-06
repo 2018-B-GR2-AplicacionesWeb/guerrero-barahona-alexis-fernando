f98600b07ba8b4dc19ea43acb95a037af626f445
Elkillax7 <aguerrero_spp@hotmail.com>
2018-11-06 12:48:45 -0500

    deber ejercicio promesa

--- 01-Javascript/deber1.js
+++ 01-Javascript/deber1.js
@@ -3,10 +3,56 @@ const fs = require('fs');
 
 
 
-const arregloStrings = ['A','B','C'];
 
-arregloStrings.forEach(
-    (string,indice)=>{
-        console.log(string);
+
+const promesaEjercicio = (arregloStrings) => {
+
+    const arregloRespuestas = [];
+    
+    return new Promise(
+        (resolve, reject) => {
+            arregloStrings.forEach(
+                (string, indice) => {
+                    const nombreArchivo = `${indice}-${string}.txt`;
+                    const contenidoArchivo = string;
+
+                    fs.writeFile(
+                        nombreArchivo,
+                        contenidoArchivo,
+                        (err) => {
+                            const respuesta = {
+                                nombreArchivo: nombreArchivo,
+                                contenidoArchivo: contenidoArchivo,
+                                error: err
+                            };
+
+                            arregloRespuestas.push(respuesta);
+                            const terminoElArreglo = arregloStrings.length === arregloRespuestas.length;
+                            if (terminoElArreglo) {
+                                resolve(arregloRespuestas);
+                            }
+                        }
+                    );
+                }
+            )
+
+        }
+    )
+}
+
+
+promesaEjercicio(['A', 'B', 'C'])
+.then(
+    (arregloRespuestas)=>{
+        console.log(arregloRespuestas);
     }
-);
+
+)
+
+.catch(
+    (error)=>{
+        console.log(error);
+    }
+)
+
+   
