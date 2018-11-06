b5af9ada522046a22b456cc51e06114428c2243b
Elkillax7 <aguerrero_spp@hotmail.com>
2018-11-06 11:47:00 -0500

    asdsad

---
+++ 01-Javascript/deber1.js
@@ -0,0 +1,12 @@
+//TRANSFORMAR LA FUNCION EJERCICIO A PROMESA
+const fs = require('fs');
+
+
+
+const arregloStrings = ['A','B','C'];
+
+arregloStrings.forEach(
+    (string,indice)=>{
+        console.log(string);
+    }
+);
