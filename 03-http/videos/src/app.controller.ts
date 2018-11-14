6a74c3529455fdaefafc884fc57f24037620ab98
Elkillax7 <aguerrero_spp@hotmail.com>
2018-11-14 15:57:03 -0500

    web

--- 03-http/videos/src/app.controller.ts
+++ 03-http/videos/src/app.controller.ts
@@ -1,12 +1,42 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, Request, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  root(): string {
-    return this.appService.root();
+
+
+
+
+
+
+
+@Controller('Usuario')//decoradores - es una funcion que se ejecuta antes de algo , este decorador recibe como el nombre del segmento //http.1192.168.1.6:3000/Usuario es deci rpone lo que se ejecutara en este segemento 
+
+
+
+
+
+
+
+export class AppController {
+
+
+
+  @Get('saludar')//aqui se define  //http.1192.168.1.6:3000/Usuario/metodoHola como le metodo lo que esta dentro dle controlador
+  saludar(): string {//metodo
+    return ('hola mundo')
   }
+
+
+  @Get('despedirse')//aqui se define  //http.1192.168.1.6:3000/Usuario/metodoHola como le metodo lo que esta dentro dle controlador
+  @HttpCode(201)
+  despedirse(): Promise<string> {//metodo
+    return new Promise<string>(
+      (resolve, reject)=>{
+        resolve('adios');
+      }
+    )
+  }
+
+
+
 }
---
+++ 03-http/videos/src/mi-codigo.ts
@@ -0,0 +1,26 @@
+const a = 'a';
+
+var khjghj = 'c';
+function f(){
+    
+}
+
+
+class Usuario{
+
+}
+
+interface UsuarioInterface{
+
+}
+
+
+//para expoertar en TS
+
+export const b = 'b';
+export const c = 'b';
+export const d = 'b';
+export const en = 'b';
+export const edfd = 'b';
+
+// solo se pone export antes del tipo variable o funcion
